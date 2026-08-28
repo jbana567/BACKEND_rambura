89821501529914ef5322119a7f741b429c976427
jbana567 <banachristy5@gmail.com>
2026-08-28 19:49:33 +0200

    update ynz murarea ho muyikore neza

--- db.js
+++
@@ -1,2 +0,0 @@
-
-// connect ou file to the mongodb compass to access our table we created there
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const connectDB = require("./src/config/db");
-
+const {ObjectId}=require("mongodb")
 const app = express();
 
 app.use(express.json());
@@ -9,13 +9,39 @@ const startServer = async () => {
     try {
         const db = await connectDB();
 
-        app.get("/", (req, res) => {
+        app.get("/", async(req, res) => {
             res.json({
                 message: "Rambura backend is running"
             });
         });
+app.get("/dashboard",async(req,res)=>{
+     const user=[];
+     const result= await db.collection("user").find().forEach((data)=>{
+        user.push(data)
+     });
+      res.json(user);
+}) 
+app.get("/dashboard/:id",async(req,res)=>{
+    const id=req.params.id;
+    const data=await db.collection("user").findOne({_id:new ObjectId(id)})
+    res.json(data);
+})
 
-        app.listen(3000, () => {
+app.post("/insert",async(req,res) =>{
+    const body=req.body
+    const results= await db.collection("user").insertMany(body);
+    res.json(results);
+})
+
+app.patch("/update/:id",async (req,res) =>{
+    const id=req.params.id;
+    const data=await db.collection("user").updateOne({_id:new ObjectId(id)},{$set:{name:req.body.name}})
+    res.json(data);
+
+})
+
+
+app.listen(3000, () => {
             console.log("Server running on port 3000");
         });
 
